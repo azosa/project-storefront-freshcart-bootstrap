f189a49f53112858aaa0072699ec2bc1f5c444c5
Paulina <azosa93@gmail,com>
2023-01-28 00:44:22 +0100

    feat: getStoreTags method added

--- src/app/services/stores.service.ts
+++ src/app/services/stores.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { StoreModel } from '../models/store.model';
+import { StoreTagModel } from '../models/store-tag.model';
 
 @Injectable({ providedIn: 'root' })
 export class StoresService {
@@ -11,7 +12,11 @@ export class StoresService {
   getAllStores(): Observable<StoreModel[]> {
     return this._httpClient.get<StoreModel[]>('https://6384fca14ce192ac60696c4b.mockapi.io/freshcart-stores');
   }
-  getOneStore(storeId:string): Observable<StoreModel[]> {
+  getOneStore(storeId: string): Observable<StoreModel[]> {
     return this._httpClient.get<StoreModel[]>(`https://6384fca14ce192ac60696c4b.mockapi.io/freshcart-stores/${storeId}`);
+  }
+
+  getStoreTags(): Observable<StoreTagModel[]> {
+    return this._httpClient.get<StoreTagModel[]>('https://6384fca14ce192ac60696c4b.mockapi.io/freshcart-store-tags');
   }
 }
