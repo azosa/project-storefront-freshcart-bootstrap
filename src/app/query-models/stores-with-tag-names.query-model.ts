fa49a3023c4475c09a2adfc16460589e51aa3aed
Paulina <azosa93@gmail,com>
2023-01-29 22:33:48 +0100

    fix: queryModel updated

--- src/app/query-models/stores-with-tag-names.query-model.ts
+++
@@ -1,7 +0,0 @@
-export interface StoresWithTagNamesQueryModel {
-  readonly name: string;
-  readonly logoUrl: string;
-  readonly distanceInMeters: number;
-  readonly id: string;
-  readonly tagNames: string[];
-}
