3439d1e36070c7c36226f42eaa07a5c42ea75dc6
Paulina <azosa93@gmail,com>
2023-01-29 22:34:22 +0100

    feat: Store products page created

--- src/app/components/store-products/store-products.component.ts
+++ src/app/components/store-products/store-products.component.ts
@@ -1,4 +1,12 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, combineLatest } from 'rxjs';
+import { filter, map, startWith, switchMap, tap } from 'rxjs/operators';
+import { ProductModel } from '../../models/product.model';
+import { StoreModel } from '../../models/store.model';
+import { ProductsService } from '../../services/products.service';
+import { StoresService } from '../../services/stores.service';
 
 @Component({
   selector: 'app-store-products',
@@ -8,4 +16,35 @@ import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class StoreProductsComponent {
+  readonly search: FormGroup = new FormGroup({ searchMarket: new FormControl() });
+
+  
+    readonly store$: Observable<StoreModel[]|any> = this._activatedRoute.params.pipe(
+      switchMap((data) => {
+       return this._storesService.getOneStore(data['storeId'])}
+     ),tap(()=>{
+      console.log(this.store$)
+     })
+    )
+
+  readonly productsList$: Observable<ProductModel[]> = combineLatest([
+    this._productsService.getAllProducts(),
+    this.search.valueChanges.pipe(startWith('')),
+    this.store$
+  ])
+  .pipe(
+    map(
+    ([products,search,store]) =>
+      search.searchMarket ? products.filter((prod) => 
+      prod.name.toLowerCase().includes(search.searchMarket.toLowerCase())&&
+      prod.storeIds.includes(store.id)
+) : products
+
+  )
+  );
+
+
+  constructor(private _productsService: ProductsService, private _activatedRoute: ActivatedRoute, private _storesService: StoresService) {
+  
+  }
 }
