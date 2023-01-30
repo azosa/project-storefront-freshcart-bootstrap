55aaa5d0d1b896d15e5686e1bd7a624b64a6a37f
Paulina <azosa93@gmail,com>
2023-01-30 12:25:42 +0100

    fix: console.log removed

--- src/app/components/category-products/category-products.component.ts
+++ src/app/components/category-products/category-products.component.ts
@@ -1,4 +1,8 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ViewEncapsulation,
+} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable, shareReplay, switchMap, tap } from 'rxjs';
 import { CategoryModel } from '../../models/category.model';
@@ -9,17 +13,20 @@ import { CategoriesService } from '../../services/categories.service';
   styleUrls: ['./category-products.component.scss'],
   templateUrl: './category-products.component.html',
   encapsulation: ViewEncapsulation.Emulated,
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CategoryProductsComponent {
-  readonly categories$: Observable<CategoryModel[]> = this._categoriesService.getAllCategories().pipe(shareReplay(1));
-  readonly category$: Observable<CategoryModel[]|any> = this._activatedRoute.params.pipe(
-    switchMap((data) => {
-     return this._categoriesService.getOneCategory(data['categoryId'])}
-   ),tap(()=>{
-    console.log(this.category$)
-   })
-  )
-  constructor(private _categoriesService: CategoriesService, private _activatedRoute: ActivatedRoute) {
-  }
+  readonly categories$: Observable<CategoryModel[]> = this._categoriesService
+    .getAllCategories()
+    .pipe(shareReplay(1));
+  readonly category$: Observable<CategoryModel[] | any> =
+    this._activatedRoute.params.pipe(
+      switchMap((data) => {
+        return this._categoriesService.getOneCategory(data['categoryId']);
+      })
+    );
+  constructor(
+    private _categoriesService: CategoriesService,
+    private _activatedRoute: ActivatedRoute
+  ) {}
 }
