7f383ecf2dfe584d8f9f146c804d9f09fb9bf014
Paulina <azosa93@gmail,com>
2023-02-05 00:08:34 +0100

    feat: wishlist queryModel created

---
+++ src/app/query-models/wishlist-product.query-model.ts
@@ -0,0 +1,9 @@
+export interface WishlistProductQueryModel {
+  readonly name: string;
+  readonly price: string;
+  readonly amount: string;
+  readonly unit: string;
+  readonly status: string;
+  readonly imgUrl: string;
+  readonly id: string;
+}
