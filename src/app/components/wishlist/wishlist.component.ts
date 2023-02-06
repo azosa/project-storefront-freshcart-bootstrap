871ee445d6823532e930abc763d6ba768e0d4a31
Paulina <azosa93@gmail,com>
2023-02-06 23:23:21 +0100

    feat: addToBasket function added

--- src/app/components/wishlist/wishlist.component.ts
+++ src/app/components/wishlist/wishlist.component.ts
@@ -3,9 +3,13 @@ import {
   Component,
   ViewEncapsulation,
 } from '@angular/core';
-import { Observable } from 'rxjs';
-import { WishlistProductQueryModel } from 'src/app/query-models/wishlist-product.query-model';
+import { combineLatest, map, Observable, tap } from 'rxjs';
+import { Router } from '@angular/router';
+import { WishlistProductQueryModel } from '../../query-models/wishlist-product.query-model';
+import { ProductModel } from '../../models/product.model';
 import { WishlistService } from '../../services/wishlist.service';
+import { BasketService } from '../../services/basket.service';
+import { ProductsService } from '../../services/products.service';
 
 @Component({
   selector: 'app-wishlist',
@@ -16,9 +20,27 @@ import { WishlistService } from '../../services/wishlist.service';
 export class WishlistComponent {
   readonly wishlistProducts$: Observable<WishlistProductQueryModel[]> =
     this._wishlistService.getAll();
-  constructor(private _wishlistService: WishlistService) {}
+
+  constructor(
+    private _wishlistService: WishlistService,
+    private _basketService: BasketService,
+    private _router: Router,
+  ) {}
   removeProd(id: string) {
     this._wishlistService.removeProdFromWishlist(id);
   }
-  addToCart(id: string) {}
+
+  addToCart(item: WishlistProductQueryModel) {
+    const el = [
+      {
+        name: item.name,
+        imgUrl: item.imgUrl,
+        quantity:1,   
+        id: item.id,
+        price:+item.price,
+      },
+    ];
+    this._basketService.addtoBasket(el[0]);
+    this._router.navigate(['/basket']);
+  }
 }
